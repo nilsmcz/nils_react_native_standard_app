d50f2dc9debb15459ea321051afc675b6a02bb18
Nils <n.morczinietz@riedel-digital.net>
2024-03-02 21:41:05 +0100

    header improvment of second title

--- app/components/HeaderPrimary.js
+++ app/components/HeaderPrimary.js
@@ -5,7 +5,7 @@ import React from 'react'
 import ArrowBack1 from '../assets/graphics/icons/arrow_back1.svg';
 import ArrowNext1 from '../assets/graphics/icons/arrow_next1.svg';
 
-export default function HeaderPrimary({ title, back, onBackClick, onNextClick, next}) {
+export default function HeaderPrimary({ titleLeft, titleRight, back, onBackClick, onNextClick, next, line}) {
     const arrowBack1 = (<ArrowBack1 width={9} height={15}/>);
     const arrowNext1 = (<ArrowNext1 width={9} height={15}/>);
 
@@ -18,32 +18,53 @@ export default function HeaderPrimary({ title, back, onBackClick, onNextClick, n
     }
 
     return (
-        <View style={{ display:"flex", width:"100%", height:"auto", justifyContent:"center", alignItems:"center"}}>
-            <View style={{display:"flex", width:"93%", paddingTop:15}}>
-                <View style={{ display:"flex", flexDirection:"row", alignItems:"center", gap:10, width:"auto"}}>
+        <>
+            {(titleLeft || back || next || titleRight) &&
+            <View style={{ display:"flex", width:"100%", height:"auto", justifyContent:"center", alignItems:"center"}}>
+                <View style={{display:"flex", width:"93%", paddingTop:13, paddingBottom:13}}>
+                    <View style={{ display:"flex", flexDirection:"row", alignItems:"center", width:"100%"}}>
 
-                    <TouchableOpacity onPress={handleBackClick} activeOpacity={1}>
-                        <View style={{ display:"flex", flexDirection:"row", alignItems:"center", gap:10, width:"auto"}}>
-                            {arrowBack1}
-                            <Text style={{  fontSize:15, fontFamily: 'Space Grotesk Medium', color:"#101010", marginBottom:2}}>{back}</Text>
-                        </View>
-                    </TouchableOpacity>
+                        {back &&
+                        <TouchableOpacity onPress={handleBackClick} activeOpacity={1}>
+                            <View style={{ display:"flex", flexDirection:"row", alignItems:"center", gap:10, width:"auto"}}>
+                                {arrowBack1}
+                                <Text style={{  fontSize:15, fontFamily: 'Space Grotesk Medium', color:"#101010", marginBottom:2}}>{back}</Text>
+                            </View>
+                        </TouchableOpacity>}
 
-                    <View style={{ display:"flex", flex:1, backgroundColor:"red"}}></View>
+                        <View style={{ display:"flex", flex:1}}></View>
 
-                    <TouchableOpacity onPress={handleNextClick} activeOpacity={1}>
-                        <View style={{ display:"flex", flexDirection:"row", alignItems:"center", gap:10, width:"auto"}}>
-                            <Text style={{  fontSize:15, fontFamily: 'Space Grotesk Medium', color:"#101010", marginBottom:2}}>{next}</Text>
-                            {arrowNext1}
-                        </View>
-                    </TouchableOpacity>
+                        {next &&
+                        <TouchableOpacity onPress={handleNextClick} activeOpacity={1}>
+                            <View style={{ display:"flex", flexDirection:"row", alignItems:"center", gap:10, width:"auto"}}>
+                                <Text style={{  fontSize:15, fontFamily: 'Space Grotesk Medium', color:"#101010", marginBottom:2}}>{next}</Text>
+                                {arrowNext1}
+                            </View>
+                        </TouchableOpacity>}
 
+                    </View>
+                    
+                    <View style={{ display:"flex", flexDirection:"row", alignItems:"center", width:"100%"}}>
+
+                        {titleLeft &&
+                        <View style={{ display:"flex", flexDirection:"row", alignItems:"center", gap:10, paddingTop:6, paddingBottom:2}}>
+                            <Text style={{  fontSize:23, fontFamily: 'Space Grotesk Bold', color:"#101010"}}>{titleLeft}</Text>
+                        </View>}
+
+                        <View style={{ display:"flex", flex:1, backgroundColor:"red"}}></View>
+
+                        {titleRight &&
+                        <View style={{ display:"flex", flexDirection:"row", alignItems:"center", gap:10, paddingTop:6, paddingBottom:2}}>
+                            <Text style={{  fontSize:23, fontFamily: 'Space Grotesk Bold', color:"#101010"}}>{titleRight}</Text>
+                        </View>}
+
+                    </View>
                 </View>
-                <View style={{ display:"flex", flexDirection:"row", alignItems:"center", gap:10, marginTop:7, marginBottom:15}}>
-                    <Text style={{  fontSize:23, fontFamily: 'Space Grotesk Bold', color:"#101010"}}>{title}</Text>
-                </View>
-            </View>
-            <View style={{display:"flex", width:"100%", height:1.2, backgroundColor:"#101010"}}></View>
-        </View>
+
+                {line &&
+                <View style={{display:"flex", width:"100%", height:1.2, backgroundColor:"#101010"}}></View>}
+                
+            </View>}
+        </>
     )
 }
